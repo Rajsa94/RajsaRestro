e497e87afff9cd39db1c4032a1378af8080d69a0
Rajsa94 <115164768+Rajsa94@users.noreply.github.com>
2022-11-22 20:36:31 +0530

    rajsa

--- db/db.js
+++ db/db.js
@@ -1,19 +1,19 @@
 // getting-started.js
 const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb+srv://Rathore:Ra9680879504%23@cluster0.1dhc6u9.mongodb.net/Pizza", {  useUnifiedTopology: true })
-// .then(()=>{
-//     console.log('connection is successful');
-// }).catch(err => {
-//     console.log('Connection failed...')
-// });
-
-mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Database connected...');
+mongoose.connect("mongodb+srv://Rathore:Ra9680879504%23@cluster0.1dhc6u9.mongodb.net/Pizza", {  useUnifiedTopology: true })
+.then(()=>{
+    console.log('connection is successful');
 }).catch(err => {
     console.log('Connection failed...')
 });
+
+// mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log('Database connected...');
+// }).catch(err => {
+//     console.log('Connection failed...')
+// });
   
   
